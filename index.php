4baafc0fc4bc72b569aea4cbbe6083858ebb8406
Guy Mann <guydmann@gmail.com>
2010-06-03 01:36:13 -0400

    consolidated layout

--- index.php
+++ index.php
@@ -158,81 +158,99 @@
 			This version uses Javascript to render the images to the new canvas element available in HTML5.
 			This page has been tested with Firefox 3.6, Opera 10.5 and Chrome<br><br>
 			To zoom on a section of the fractal click and drag to highlight the region.  You can reload the page or click <a href="./">here</a><br>
-			<canvas id="theCanvas" width="<?php echo $width; ?>" height="<?php echo ($width+20); ?>">Fallback content, in case the browser does not support Canvas.</canvas>
-			<table> 
-		<tr>
-		<td> 
-		<table> 
-			<tr><td> colorscheme:</td><td><select name="colorscheme" id="colorscheme"> 
-				<option <? if ($colorscheme == 0) {  print "selected ";} ?> value=0>Simple</option> 
-				<option <? if ($colorscheme == 1) {  print "selected ";} ?> value=1> 5 Color Cyclic 1</option> 
-				<option <? if ($colorscheme == 2) {  print "selected ";} ?> value=2>5 Color Cyclic 2</option> 
-				<option <? if ($colorscheme == 3) {  print "selected ";} ?> value=3>12 Color Cyclic 1</option> 
-				<option <? if ($colorscheme == 4) {  print "selected ";} ?> value=4>36 Color Cyclic</option> 
-				<option <? if ($colorscheme == 5) {  print "selected ";} ?> value=5>HSV 0-360</option> 
-				<option <? if ($colorscheme == 6) {  print "selected ";} ?> value=6>HSV 360-0</option> 
-				<option <? if ($colorscheme == 7) {  print "selected ";} ?> value=7>HSV with static modulus</option> 
-				<option <? if ($colorscheme == 8) {  print "selected ";} ?> value=8>HSV with percentage modulus</option> 
-				<option <? if ($colorscheme == 9) {  print "selected ";} ?> value=9>HSV with percentage modulus 2</option> 
-				<option <? if ($colorscheme == 99) {  print "selected ";} ?> value=99>2 Color Black and White</option> 
-				</select> 
-			<td></tr> 
-			<tr>
-				<td> algorithm:</td><td><select name="algorithm" id="algorithm" onchange="resetcoords();"> 
-				<option <?php if ($algorithm == 0) {  print "selected ";} ?> value=0>Mandelbrot (Quadratic)</option> 
-				<option <?php if ($algorithm == 1) {  print "selected ";} ?> value=1>Julia (Quadratic)</option> 
-				<option <?php if ($algorithm == 2) {  print "selected ";} ?> value=2>Burning Ship</option> 
-				<option <?php if ($algorithm == 3) {  print "selected ";} ?> value=3>Newton</option> 
-				<option <?php if ($algorithm == 4) {  print "selected ";} ?> value=4>Star</option> 
-				<option <?php if ($algorithm == 5) {  print "selected ";} ?> value=5>Phoenix, Julia type</option> 
-				<option <?php if ($algorithm == 6) {  print "selected ";} ?> value=6>Phoenix, Mandelbrot type</option> 
-				<option <?php if ($algorithm == 7) {  print "selected ";} ?> value=7>Cubic Mandelbrot</option> 
-				<option <?php if ($algorithm == 8) {  print "selected ";} ?> value=8>Quartic Mandelbrot</option> 
-				<option <?php if ($algorithm == 9) {  print "selected ";} ?> value=9>Cubic Julia</option> 
-				<option <?php if ($algorithm == 10) {  print "selected ";} ?> value=10>Quartic Julia</option> 
-				<option <?php if ($algorithm == 11) {  print "selected ";} ?> value=11>Cubic Julia Experimental</option> 
-				<option <?php if ($algorithm == 12) {  print "selected ";} ?> value=12>Mandelbrot Interior Coloring</option> 
-				<option <?php if ($algorithm == 13) {  print "selected ";} ?> value=13>Mandelbrot Interior Coloring 2</option> 
-				<option <?php if ($algorithm == 99) {  print "selected ";} ?> value=99>Blank</option> 
-				</select> 
+			
+			<table>
+				<tr>
+				<td>
+					<canvas id="theCanvas" width="<?php echo $width; ?>" height="<?php echo ($width+20); ?>">Fallback content, in case the browser does not support Canvas.</canvas>
 				</td>
-			</tr> 
-			<tr> 
-				<td>URL:</td><td><input type="text" size="50" name="URL" id="URL" value="<?php echo $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];?>"> </td>
+				<td>
+				<table> 				
+					<tr>
+						<td><input type="submit" value="Redraw"  onclick="redraw();">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+						<td>&nbsp;</td>
+						<td>
+							<input type="submit" value="Zoom Out"  onclick="zoom_out();">
+						</td>
+						<td>
+							<input type="submit" value="Reset Coords"  onclick="resetcoords();redraw();">
+						</td>
+					</tr>
+					<tr>
+						<td> algorithm:</td>
+						<td colspan=3><select name="algorithm" id="algorithm" onchange="resetcoords();"> 
+						<option <?php if ($algorithm == 0) {  print "selected ";} ?> value=0>Mandelbrot (Quadratic)</option> 
+						<option <?php if ($algorithm == 1) {  print "selected ";} ?> value=1>Julia (Quadratic)</option> 
+						<option <?php if ($algorithm == 2) {  print "selected ";} ?> value=2>Burning Ship</option> 
+						<option <?php if ($algorithm == 3) {  print "selected ";} ?> value=3>Newton</option> 
+						<option <?php if ($algorithm == 4) {  print "selected ";} ?> value=4>Star</option> 
+						<option <?php if ($algorithm == 5) {  print "selected ";} ?> value=5>Phoenix, Julia type</option> 
+						<option <?php if ($algorithm == 6) {  print "selected ";} ?> value=6>Phoenix, Mandelbrot type</option> 
+						<option <?php if ($algorithm == 7) {  print "selected ";} ?> value=7>Cubic Mandelbrot</option> 
+						<option <?php if ($algorithm == 8) {  print "selected ";} ?> value=8>Quartic Mandelbrot</option> 
+						<option <?php if ($algorithm == 9) {  print "selected ";} ?> value=9>Cubic Julia</option> 
+						<option <?php if ($algorithm == 10) {  print "selected ";} ?> value=10>Quartic Julia</option> 
+						<option <?php if ($algorithm == 11) {  print "selected ";} ?> value=11>Cubic Julia Experimental</option> 
+						<option <?php if ($algorithm == 12) {  print "selected ";} ?> value=12>Mandelbrot Interior Coloring</option> 
+						<option <?php if ($algorithm == 13) {  print "selected ";} ?> value=13>Mandelbrot Interior Coloring 2</option> 
+						<option <?php if ($algorithm == 99) {  print "selected ";} ?> value=99>Blank</option> 
+						</select> 
+						</td>
+					</tr> 
+					<tr>
+						<td> colorscheme:</td>
+						<td colspan=3><select name="colorscheme" id="colorscheme"> 
+						<option <? if ($colorscheme == 0) {  print "selected ";} ?> value=0>Simple</option> 
+						<option <? if ($colorscheme == 1) {  print "selected ";} ?> value=1> 5 Color Cyclic 1</option> 
+						<option <? if ($colorscheme == 2) {  print "selected ";} ?> value=2>5 Color Cyclic 2</option> 
+						<option <? if ($colorscheme == 3) {  print "selected ";} ?> value=3>12 Color Cyclic 1</option> 
+						<option <? if ($colorscheme == 4) {  print "selected ";} ?> value=4>36 Color Cyclic</option> 
+						<option <? if ($colorscheme == 5) {  print "selected ";} ?> value=5>HSV 0-360</option> 
+						<option <? if ($colorscheme == 6) {  print "selected ";} ?> value=6>HSV 360-0</option> 
+						<option <? if ($colorscheme == 7) {  print "selected ";} ?> value=7>HSV with static modulus</option> 
+						<option <? if ($colorscheme == 8) {  print "selected ";} ?> value=8>HSV with percentage modulus</option> 
+						<option <? if ($colorscheme == 9) {  print "selected ";} ?> value=9>HSV with percentage modulus 2</option> 
+						<option <? if ($colorscheme == 99) {  print "selected ";} ?> value=99>2 Color Black and White</option> 
+						</select> 
+					<td></tr> 
+					<tr> 
+						<td>URL:</td><td colspan=3><input type="text" size="30" name="URL" id="URL" value="<?php echo $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];?>"> </td>
+					</tr>
+
+
+					<tr>
+						<td> Left X Coord</td><td><input type="text" size="4" name="lx" id="lx" value="<?php echo $lx; ?>"> </td>
+						<td> Right X Coord</td><td><input type="text" size="4" name="rx" id="rx" value="<?php echo $rx; ?>"> </td>
+					</tr> 
+					<tr></tr> 
+					<tr>
+						<td> Top Y Coord</td><td><input type="text" size="4" name="ty" id="ty" value="<?php echo $ty; ?>"> </td>
+						<td> Bottom Y Coord</td><td><input type="text" size="4" name="by" id="by" value="<?php echo $by; ?>"> </td>
+					<tr>
+						<td>&nbsp; </td>
+						<td>&nbsp; </td>
+						<td>Width: </td><td><input type="text" size="2" name="width" id="width" value="<?php echo $width; ?>"> </td>
+					</tr> 
+					<tr id="Julia_args">
+						<?php
+						if ($algorithm == 1 or $algorithm == 5 or $algorithm == 1 or $algorithm == 9 or $algorithm == 10 or $algorithm == 11) {
+						?>
+							<td> C(real):</td><td><input type="text" size="4" value="<?php echo $cr; ?>" name="cr" id="cr"> </td><td> C(imaginary):</td><td><input type="text" size="4" value="<?php echo $ci; ?>" name="ci" id="ci"></td>
+						<?php
+						} else {
+						?>
+							<td></td><td><input type="hidden" size="4" value="" name="cr" id="cr"><td><td></td><td><input type="hidden" size="4" value="" name="ci" id="ci"></td>
+						<?php
+						}
+						?>
+					</tr> 
+				</table>
+				</td>
+				</tr>
+			</table>
+			</td>
 			</tr>
 		</table>
-		</td> 
-		<td>
-		<table> 
-			<tr>
-				<td> Left X Coord</td><td><input type="text" size="4" name="lx" id="lx" value="<?php echo $lx; ?>"> </td>
-				<td> Right X Coord</td><td><input type="text" size="4" name="rx" id="rx" value="<?php echo $rx; ?>"> </td>
-			</tr> 
-			<tr></tr> 
-			<tr>
-				<td> Top Y Coord</td><td><input type="text" size="4" name="ty" id="ty" value="<?php echo $ty; ?>"> </td>
-				<td> Bottom Y Coord</td><td><input type="text" size="4" name="by" id="by" value="<?php echo $by; ?>"> </td>
-			<tr>
-				<td><br><br> </td>
-				<td>Width: </td><td><input type="text" size="2" name="width" id="width" value="<?php echo $width; ?>"> </td>
-			</tr> 
-			<tr id="Julia_args">
-				<?php
-				if ($algorithm == 1 or $algorithm == 5 or $algorithm == 1 or $algorithm == 9 or $algorithm == 10 or $algorithm == 11) {
-				?>
-					<td> C(real):</td><td><input type="text" size="4" value="<?php echo $cr; ?>" name="cr" id="cr"> </td><td> C(imaginary):</td><td><input type="text" size="4" value="<?php echo $ci; ?>" name="ci" id="ci"></td>
-				<?php
-				} else {
-				?>
-					<td></td><td><input type="hidden" size="4" value="" name="cr" id="cr"><td><td></td><td><input type="hidden" size="4" value="" name="ci" id="ci"></td>
-				<?php
-				}
-				?>
-			</tr> 
-		</table>
-		</td></tr>
-		</table>
-		<input type="submit" value="Zoom Out"  onclick="zoom_out();"><input type="submit" value="Reset Coords"  onclick="resetcoords();redraw();"><input type="submit" value="Redraw"  onclick="redraw();">
 		</td>
 		<td width="5%"></td>
 		</tr>
